9a67105d5714a7f46d07026d4c9407edee5af950
winstxnhdw <winstxnhdw@gmail.com>
2023-04-20 13:39:44 +0800

    refactor: use slice instead

--- src/get_requests.rs
+++ src/get_requests.rs
@@ -5,7 +5,7 @@ async fn get_request(url: &String, client: &Client) -> Result<String, Error> {
     client.get(url).send().await?.text().await
 }
 
-pub async fn get_requests(endpoints: &Vec<String>) -> Result<Vec<String>, Error> {
+pub async fn get_requests(endpoints: &[String]) -> Result<Vec<String>, Error> {
     let client = Client::new();
 
     endpoints
