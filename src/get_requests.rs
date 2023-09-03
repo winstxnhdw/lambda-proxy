2c20049394ff650a0863010ee1594c021eed030d
winstxnhdw <winstxnhdw@gmail.com>
2023-09-03 21:35:35 +0800

    feat: add `User-Agent` header

--- src/get_requests.rs
+++ src/get_requests.rs
@@ -1,8 +1,15 @@
 use futures::{stream::FuturesUnordered, TryStreamExt};
+use reqwest::header::USER_AGENT;
 use reqwest::{Client, Error};
 
 async fn get_request(url: &String, client: &Client) -> Result<String, Error> {
-    client.get(url).send().await?.text().await
+    client
+        .get(url)
+        .header(USER_AGENT, "lambda-proxy")
+        .send()
+        .await?
+        .text()
+        .await
 }
 
 pub async fn get_requests(endpoints: &[String]) -> Result<Vec<String>, Error> {
